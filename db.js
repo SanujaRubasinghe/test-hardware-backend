3f04079036cd64d3ea40266ce5a59c9a17855c06
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2025-05-03 17:06:33 +0530

    update db connectin string

--- db.js
+++ db.js
@@ -14,11 +14,12 @@ require('dotenv').config()
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false, 
     },
