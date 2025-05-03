5939b7d77849fcaa6c03148c9797df8989f3173b
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2025-05-03 17:59:43 +0530

    fix auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,18 +8,18 @@ require("dotenv").config()
 router.post("/login", async (req, res) => {
     const {username, password} = req.body
     try {
-        const [rows] = await pool.query("select * from usrtbl where usrname=?", [username])
-        if (rows[0].length === 0) {
+        const result = await pool.query("select * from usrtbl where usrname=?", [username])
+        if (result.rowCount === 0) {
             return res.status(400).json({message: "User not found"})
         }
 
         //change this to bcrypt.compare after hashing the passwords
-        const isPasswordValid = rows[0].usrpassword === password
+        const isPasswordValid = result.rows[0].usrpassword === password
         if (!isPasswordValid) {
             return res.status(400).json({message: "Invalid credentials"})
         }
 
-        const token = jwt.sign({id: rows[0].id}, process.env.JWT_KEY, {expiresIn: "5h"})
+        const token = jwt.sign({id: result.rows[0].id}, process.env.JWT_KEY, {expiresIn: "5h"})
 
         res.cookie("token", token, {httpOnly: true, secure: false, sameSite: "lax"})
         return res.json({message: "Login Successful", usertoken: token })
@@ -39,8 +39,8 @@ router.post("/logout", (req, res) => {
 })
 
 router.get("/profile", authenticateToken, async (req, res) => {
-    const [rows] = await pool.query("select id, usrname, usremail from usrtbl where id=?", [req.user.id])
-    res.json(rows[0])
+    const result = await pool.query("select id, usrname, usremail from usrtbl where id=?", [req.user.id])
+    res.json(result.rows[0])
 })
 
 module.exports = router
